ba895b9ae848c95cd85095f4ba90402a5924cee5
zsx <thinkernel@gmail.com>
2016-04-28 12:08:14 +0000

    Support docker network

--- createNexus.sh
+++ createNexus.sh
@@ -3,6 +3,7 @@ set -e
 NEXUS_NAME=${NEXUS_NAME:-nexus}
 NEXUS_VOLUME=${NEXUS_VOLUME:-nexus-volume}
 NEXUS_IMAGE_NAME=${NEXUS_IMAGE_NAME:-openfrontier/nexus}
+CI_NETWORK=${CI_NETWORK:-ci-network}
 
 #Create nexus volume.
 if [ -z "$(docker ps -a | grep ${NEXUS_VOLUME})" ]; then
@@ -15,7 +16,7 @@ fi
 #Start nexus
 docker run \
 --name ${NEXUS_NAME} \
--p 8081:8081 \
+--net ${CI_NETWORK} \
 --volumes-from ${NEXUS_VOLUME} \
 -e CONTEXT_PATH=/nexus \
 -d ${NEXUS_IMAGE_NAME}
